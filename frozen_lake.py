521dfa27df846aa8fa069617415acaa711a5c721
simongustavsson31-gif <simon.gustavsson31@gmail.com>
2026-09-25 14:41:39 +0200

    Enhance Q-learning implementation with epsilon-greedy strategy and convergence criteria

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 from gymnasium.wrappers import TransformReward
-#import qlearning 
-
+import qlearning
+import numpy as np
 # https://gymnasium.farama.org/environments/toy_text/frozen_lake/
 env = gym.make('FrozenLake-v1', desc=None, map_name="8x8", is_slippery=False, render_mode=None)
 env = TransformReward(env, lambda r: 1 if r == 1 else r-0.04)
@@ -10,18 +10,34 @@ observation, info = env.reset(seed=42)
 action = env.action_space.sample()
 print("Starting")
 terminated_i = 0
-
+Q_table = qlearning.QTable(0.99, 0.1, 0.5, env.action_space.n, env.observation_space.n)
 ### TRAINING LOOP
 # In this loop your agent will be trained without showing the run
 # To stop this training loop you can pres ctrl+c in the terminal. This will start the testing loop below
 # You can implement a stop criteria if you want
 try:
+    convergence = 0.00001
+    stable_episodes = 0
+    previous_Q_table = Q_table.Q.copy()
     while True:
         observation, reward, terminated, truncated, info = env.step(action)
-        action = env.action_space.sample()  ### <-- This line samples a random action for from the environment. Replace this with your Qlearning algorithm ###
+        action = Q_table.update_Q(observation, reward)  ### <-- This line samples a random action for from the environment. Replace this with your Qlearning algorithm ###
 
         if terminated or truncated:         ### <-- This condition triggers if the run is terminated, as it the agent reaches a goal or falls into a hole
             terminated_i = terminated_i + 1
+
+
+            diffrence = np.abs(Q_table.Q - previous_Q_table).max()
+            if diffrence < convergence:
+                stable_episodes += 1
+            else:
+                stable_episodes = 0
+            if stable_episodes >= 1000:
+                print("Q-table has converged after", terminated_i, "episodes.")
+                break
+
+            previous_Q_table = Q_table.Q.copy()
+
 
             if reward > 0:                  ### <-- This condition triggers when the agent finds the goal. You are free to debug and print how you want
                 print("Found goal at iteration", terminated_i)
@@ -37,12 +53,21 @@ except KeyboardInterrupt:
 # To stop this testing loop, press ctrl+c in the terminal.
 env_test = gym.make('FrozenLake-v1', desc=None, map_name="8x8", is_slippery=False, render_mode='human')
 observation, info = env_test.reset(seed=42)
+
+
+Q_table.set_epsilon_value(0.0)  # Set epsilon to 0 to always take the best action
+episodes = 0
 try:
     while True:
         observation, reward, terminated, truncated, info = env_test.step(action)
-        action = env_test.action_space.sample()  ### <-- This line samples a random action for from the environment. Replace this with your optimal action calculation ###
+        action = Q_table.update_Q(observation, reward)  ### <-- This line samples a random action for from the environment. Replace this with your optimal action calculation ###
 
         if terminated or truncated:
+            episodes += 1
+            print("Episode", episodes, "ended with reward", reward)
+            if episodes >= 10:
+                print("Test ended after 15 episodes")
+                break
             observation, info = env_test.reset()
 
 except KeyboardInterrupt:
--- qlearning.py
+++ qlearning.py
@@ -17,36 +17,55 @@ class QTable:
 
 
         # s, a, r , the previous state, action, and reward, initially null
-        self.previous_Q_index = np.NaN
-        self.previous_action = np.NaN
-        self.previous_reward = np.NaN
+        self.previous_Q_index = np.nan
+        self.previous_action = np.nan
+        self.previous_reward = np.nan
 
         # Initiate Q-tables here
-
+        self.Q = np.zeros((64, 4))
 
     def get_Q_index(self, coded_state):
-        return # Return the index of a row in the Qtable
+        return coded_state  # Return the index of a row in the Qtable ?????????
 
     def get_Q_actions(self, Q_index):
-        return # Return action for a row in the Qtable
+        return self.Q[Q_index]  # Return action for a row in the Qtable
 
     def set_Q(self, Q_index, action, new_Q):
         # Set calculated Q value for a state action pair
+        self.Q[Q_index, action] = new_Q
 
     def get_max_Q(self, Q_index):
-        return # Maxumum Q value in a row
+        return np.max(self.Q[Q_index])  # Maximum Q value in a row
 
     def epsilon_greedy(self, Q_index):
 
-        return # Either best action or random action
-        
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.action_size)
+        else:
+            return self.get_best_action(Q_index)
 
     def get_best_action(self, current_state):
 
-        return # Best action
+        return np.argmax(self.Q[current_state]) # Best action
 
     def update_Q(self, current_state, current_reward):
         # Algorithm used from "Artificial Intelligence A Modern Approach" by Stuart Russell and Peter Norvig
         # Q-Learning-Agent, page 844, figure 21.8
+        
+        if not np.isnan(self.previous_Q_index):
 
-        return # Action to take for next step AFTER epsilon greedy has been used
+            old_Q_value = self.get_Q_actions(self.previous_Q_index)[self.previous_action]
+            new_Q_value = old_Q_value + self.alpha * (current_reward + self.gamma * self.get_max_Q(current_state) - old_Q_value)
+            self.set_Q(self.previous_Q_index, self.previous_action, new_Q_value)
+
+        next_action = self.epsilon_greedy(current_state)
+
+
+        self.previous_Q_index = self.get_Q_index(current_state)
+        self.previous_action = next_action
+        self.previous_reward = current_reward
+
+        return next_action  # Action to take for next step AFTER epsilon greedy has been used
+    def set_epsilon_value(self, epsilon_value):
+        self.epsilon = epsilon_value
+        return 
